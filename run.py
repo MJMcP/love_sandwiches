0009da1bc5d6084332d3286fd337c2c2f60463f4
MJMcP <maggiejury@gmail.com>
2022-10-01 21:20:50 +0000

    Add callculated surplus data to surplus worksheet

--- run.py
+++ run.py
@@ -27,9 +27,9 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60\n")
   
         data_str = input("Enter your data here: ")
-        sales_data = data_str.split(',')
-        validate_data(sales_data)
-
+        
+        sales_data = data_str.split(",")
+        
         if validate_data(sales_data):
             print("Data is valid!")
             break
@@ -63,7 +63,18 @@ def update_sales_worksheet(data):
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
-    print("Sales worksheeet updated successfully.\n")
+    print("Sales worksheet updated successfully.\n")
+
+
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the list data provided
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully.\n")
+
 
 def calculate_surplus_data(sales_row):
     """
@@ -76,7 +87,13 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
 
 
 def main():
@@ -86,7 +103,9 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
+    
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
